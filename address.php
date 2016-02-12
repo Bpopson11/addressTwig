aaa1305afb5170a71bbcf3da35bc8527b0a5c7b8
Brianna Popson <bpopson@gmail.com>
2016-02-12 08:16:38 -0800

    create contact class with private properties

--- address.php
+++ address.php
@@ -1,6 +1,45 @@
 <?php
-class 
+class Contact
 {
+    private $name;
+    private $phone;
+    private $address;
 
+    function __construct($name, $phone, $address)
+    {
+        $this->name = $name;
+        $this->phone = $phone;
+        $this->address = $address;
+    }
+
+    function setName($new_name)
+    {
+        $this->name = (string) $new_name;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setPhone($new_phone)
+    {
+        $this->phone = (string) $new_phone;
+    }
+
+    function getPhone()
+    {
+        return $this->phone;
+    }
+
+    function setAddress($new_address)
+    {
+        $this->address = (string) $new_address;
+    }
+
+    function getAddress()
+    {
+        return $this->address;
+    }
 }
 ?>
